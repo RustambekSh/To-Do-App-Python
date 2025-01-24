08725a746514cd6a6408e2cea2eb4ba5026e6256
Rustambek Shodiev <156027859+RustambekSh@users.noreply.github.com>
2025-01-24 15:33:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from todo_app import ToDoApp
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setStyle("Fusion")  # Modern style
+    app.setStyle("Fusion")  
     window = ToDoApp()
     window.show()
     
