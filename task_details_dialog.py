0fdeee6363e863f7b1a8eb02e1e4f48bf430a331
Rustambek Shodiev <156027859+RustambekSh@users.noreply.github.com>
2025-02-21 19:10:16 -0600

    Update task_details_dialog.py

--- task_details_dialog.py
+++ task_details_dialog.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QTextEdit, QDateEdit, QComboBox, QLineEdit, QLabel, QPushButton
 from PyQt5.QtCore import QDate
 from styles import * 
-  
+   
 class TaskDetailsDialog(QDialog):
     def __init__(self, task_text="", due_date=QDate.currentDate(), priority="Medium", description="", tags="", parent=None):
         super().__init__(parent)
